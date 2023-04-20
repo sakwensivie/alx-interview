42eb67471d5d8d6933203c8f05f87c21443cd885
Samuel Akwensivie <sakwnesivie@gmail.com>
2023-04-20 11:02:30 +0000

    correction of indentation errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,15 +2,16 @@
 
 '''This is a module for pascal_triangle function'''
 
-def pascal_triangle(n):
-	'''This function returns a list of lists of integers'''
 
-	if n <= 0:
-		return []
-	triangle = [[1]]
-	for i in range(1, n):
-		triangle.append([1])
-		for j in range(1, i):
-			triangle[i].append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-		triangle[i].append(1)
-	return triangle
+def pascal_triangle(n):
+    '''This function returns a list of lists of integers'''
+
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        triangle.append([1])
+        for j in range(1, i):
+            triangle[i].append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle[i].append(1)
+    return triangle
